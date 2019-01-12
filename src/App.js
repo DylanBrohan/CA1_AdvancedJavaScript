eff1150b327d404415128b079fc0d87a09c66a47
Dylan <dylan@Dylans-MacBook-Pro.local>
2019-01-12 18:33:14 +0000

    Deployed to Heroku

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+
+let defaultTextColor = "#000";
+
+class Music extends Component {
+  render() {
+    return (
+      <div style={{ width: "40%", display: "inline-block" }}>
+        <h2 style={{ color: "#defaultTextColor" }}>Number Text</h2>
+      </div>
+    );
+  }
+}
+
+class Filter extends Component {
+  render() {
+    return (
+      <div>
+        <img />
+        <input type="text" />
+        Filter
+      </div>
+    );
+  }
+}
+
+class Playlist extends Component {
+  render() {
+    return (
+      <div
+        style={{
+          color: defaultTextColor,
+          display: "inline-block",
+          width: "25%"
+        }}
+      >
+        <img />
+        <h3>Playlist Name</h3>
+        <ul>
+          <li>Song1</li>
+          <li>Song2</li>
+          <li>Song3</li>
+        </ul>
+      </div>
+    );
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Title</h1>
+        <Music />
+        <Music />
+        <Filter />
+        <Playlist />
+        <Playlist />
+        <Playlist />
       </div>
     );
   }
